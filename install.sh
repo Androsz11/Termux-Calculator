e2fdf43c12cbce07ee342b3150859c62028873d5
Androsz11 <73430696+Androsz11@users.noreply.github.com>
2020-11-26 16:37:17 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -e "\033[1;33mInstallation Started.\e[0m"
+sleep 1
+cp Calc $PREFIX/bin
+chmod +x Calc
+echo -e "\033[0;32mInstallation completed.\e[0m"
+echo
+echo -e "\033[0;32mNow type Calc to run it.\e[0m"
